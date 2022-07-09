9a8c8675252aecbddebb9fd503683fcd7bd22c79
yuminlee <dorayuminlee@gmail.com>
2022-07-10 08:02:25 +0900

    [feat] 메뉴바&하단 컨텐츠 공간 구현

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 function handleClickProfilePicture() {
-  document.getElementById("picture-large").style.display="block";
+  document.getElementById("picture-large").style.display = "block";
 }
 
 function handleClickClosePicture() {
-  document.getElementById("picture-large").style.display="none";
+  document.getElementById("picture-large").style.display = "none";
+}
+
+function changeMenuColor(n) {
+  for (let i = 1; i <= 3; i++) {
+    if (i == n) {
+      document.getElementById(`menu-${i}`).style.color = "var(--menu-color)";
+    } else {
+      document.getElementById(`menu-${i}`).style.color = "black";
+    }
+  }
+}
+
+function handleSlider(n) {
+  changeMenuColor(n);
+  document.getElementById("menu-slider").style.transform = `translateX(calc(${
+    n - 1
+  } * 100px))`;
 }
 
 // TODO: 게시물/정보/bgm/언어 변경/dark mode
