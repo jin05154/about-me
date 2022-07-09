5bfd8355f46f3208f31c12061c822d46226e6705
yuminlee <dorayuminlee@gmail.com>
2022-07-10 04:42:16 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function handleClickProfilePicture() {
+  document.getElementById("picture-large").style.display="block";
+}
+
+function handleClickClosePicture() {
+  document.getElementById("picture-large").style.display="none";
+}
+
+// TODO: 게시물/정보/bgm/언어 변경/dark mode
