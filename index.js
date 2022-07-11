7701058baeb6653f0340dea71695d6ab670fb538
yuminlee <dorayuminlee@gmail.com>
2022-07-11 17:39:56 +0900

    [style] 폰트, 색 등의 style 수정

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+// dark mode toggle switch
+let checkbox = document.querySelector('input[type="checkbox"]');
+let checkedMenu = 0;
+
+// TODO: addEventListener/정보/nav 아이콘/bgm/언어 변경/dark mode
+
 function handleClickProfile() {
   document.getElementById("picture-large").style.display = "block";
 }
@@ -7,13 +13,24 @@ function handleCloseProfile() {
 }
 
 function changeMenu(n) {
+  checkedMenu = n;
   for (let i = 1; i <= 3; i++) {
-    if (i == n) {
-      document.getElementById(`menu-${i}`).style.color = "var(--menu-color)";
-      document.getElementById(`content-${i}`).style.display = "contents";
+    if (checkbox.checked) {
+      if (i == n) {
+        document.getElementById(`menu-${i}`).style.color = "var(--menu-color)";
+        document.getElementById(`content-${i}`).style.display = "contents";
+      } else {
+        document.getElementById(`menu-${i}`).style.color = "var(--dark-mode-text)";
+        document.getElementById(`content-${i}`).style.display = "none";
+      }
     } else {
-      document.getElementById(`menu-${i}`).style.color = "black";
-      document.getElementById(`content-${i}`).style.display = "none";
+      if (i == n) {
+        document.getElementById(`menu-${i}`).style.color = "var(--menu-color)";
+        document.getElementById(`content-${i}`).style.display = "contents";
+      } else {
+        document.getElementById(`menu-${i}`).style.color = "var(--secondary-text)";
+        document.getElementById(`content-${i}`).style.display = "none";
+      }
     }
   }
 }
@@ -34,4 +51,33 @@ function handleCloseGallery() {
   document.getElementById("gallery-large").style.display = "none";
 }
 
-// TODO: 정보/nav 아이콘/bgm/언어 변경/dark mode
+// dark mode
+// document.addEventListener("DOMContentLoaded", function () {
+//   checkbox.addEventListener("change", function () {
+//     if (checkbox.checked) {
+//       document.body.classList.add("dark-mode");
+//       switchToDark(document.getElementById("profile"));
+//       document.getElementById("contents").style.backgroundColor = "var(--dark-mode-background-2)";
+//       // document.querySelectorAll("nav").forEach((el) => switchToDark(el));
+//       document.querySelectorAll("article").forEach((el) => switchToDark(el));
+//     } else {
+//       document.body.classList.remove("dark-mode");
+//       switchToLight(document.getElementById("profile"));
+//       document.getElementById("contents").style.backgroundColor = "var(--content-background)";
+//       // document.querySelectorAll("nav").forEach((el) => switchToLight(el));
+//       document.querySelectorAll("article").forEach((el) => switchToLight(el));
+//     }
+//   });
+// });
+
+// function switchToDark(el) {
+//   el.style.backgroundColor = "var(--dark-mode-background-1)";
+//   el.style.color = "var(--dark-mode-text)";
+//   el.style.borderBottom = "1px solid var(--dark-mode-border)";
+// }
+
+// function switchToLight(el) {
+//   el.style.backgroundColor = "white";
+//   el.style.color = "black";
+//   el.style.borderBottom = "1px solid var(--nav-border-color)";
+// }
